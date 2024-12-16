d44e6233bd0538ae02cdad230d52526d87c2ccc8
AugieAud <auds@missionreadyhq.com>
2024-12-16 15:40:53 +1300

    created folders and created auctionItem model

---
+++ models/auctionItem.js
@@ -0,0 +1,13 @@
+//define the structure of the data. Schema = blueprint
+
+const mongoose = require("mongoose");
+
+//create a new schema containing the 4 fields that each documentaion in the collection must have, title, description, start price and reserve price
+const auctionItemSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  start_price: { type: Number, required: true },
+  reserve_price: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("AuctionItem", auctionItemSchema);
